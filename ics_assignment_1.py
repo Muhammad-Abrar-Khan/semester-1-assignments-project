8b2d421d6bc5ab8a24c5a5b2d11568311cc70f7b
makabrarkhan110@gmail.com <makabrarkhan110@gmail.com>
2021-09-07 17:26:51 +0500

    Added more python files

---
+++ ics_assignment_1.py
@@ -0,0 +1,14 @@
+# a=1
+# t=0
+# n=0
+# while (a>0):
+#     a = float(input("enter receipt amount : "))
+#     if (a>0):
+#         t=t+a
+#         n=n+1
+# print("total amount = " , t)
+# print("count of receipts is" , n)
+
+#it takes input numbers i.e. receipt amount & prints the total amount & count of receipts of the value which
+
+
---
+++ ics_assignment_4.py
@@ -0,0 +1,30 @@
+
+
+# a=1
+# n=0
+# t=0
+# p=0
+# text=input("Do you want to run this code or want to end it?\n")
+# if ("run this code"  in text):
+#     while (a>0):
+#         a = float(input("enter +ve no : "))
+#         if(a>0):
+#             t=t+a
+#     print("total of +ve no is " , t)
+#     while (a<0):
+#         a = float(input("enter -ve no : "))
+#         if(a<0):
+#             n=n+1
+#     print("count of -ve no is " , n)
+#     while (a==0):
+#         a = float(input("enter zero (O) : "))
+#         if(a==0):
+#             p=p+1
+#     print("count of 0 is " , p)
+# else:
+#     print("program ended")
+
+
+
+
+
