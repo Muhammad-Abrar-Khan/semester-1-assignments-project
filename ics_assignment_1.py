9b1e6a7ab2baf5329b2d5184b56b8dedb55790b8
makabrarkhan110@gmail.com <makabrarkhan110@gmail.com>
2021-09-09 23:41:32 +0500

    Initial commit

--- ics_assignment_1.py
+++ ics_assignment_1.py
@@ -1,14 +1,17 @@
-# a=1
-# t=0
-# n=0
-# while (a>0):
-#     a = float(input("enter receipt amount : "))
-#     if (a>0):
-#         t=t+a
-#         n=n+1
-# print("total amount = " , t)
-# print("count of receipts is" , n)
+# It takes input numbers (until user entered "0") i.e. receipt amount & prints the total amount & total count of receipts i.e. total inputs except 0.
+a=1
+t=0
+n=0
+while (a>0):
+    a = float(input("enter receipt amount : "))
+    if (a>0):
+        t=t+a
+        n=n+1
+print("total amount = " , t)
+print("count of receipts is" , n)
 
-#it takes input numbers i.e. receipt amount & prints the total amount & count of receipts of the value which
+
+
+
 
 
--- ics_assignment_4.py
+++ ics_assignment_4.py
@@ -1,29 +1,32 @@
+# If you want to run this code enter "yes" else enter anything it will end this code. This program will take input +ve,-ve and 0 and will print the total of inputs i.e. sum of all inputs.
+# Note that while giving input +ve no if you enter 0 or -ve no so it will break the loop and will print total count of +ve inputs and will move on to the next step.
+# Same goes for -ve and 0.
 
-
-# a=1
-# n=0
-# t=0
-# p=0
-# text=input("Do you want to run this code or want to end it?\n")
-# if ("run this code"  in text):
-#     while (a>0):
-#         a = float(input("enter +ve no : "))
-#         if(a>0):
-#             t=t+a
-#     print("total of +ve no is " , t)
-#     while (a<0):
-#         a = float(input("enter -ve no : "))
-#         if(a<0):
-#             n=n+1
-#     print("count of -ve no is " , n)
-#     while (a==0):
-#         a = float(input("enter zero (O) : "))
-#         if(a==0):
-#             p=p+1
-#     print("count of 0 is " , p)
-# else:
-#     print("program ended")
-
+a=1
+n=0
+t=0
+p=0
+while True:
+    text=input("Do you want to run this code or want to end it?\n")
+    if ("yes"  in text):
+        while (a>0):
+            a = float(input("enter +ve no : "))
+            if(a>0):
+                t=t+a
+        print("total of +ve no is " , t)
+        while (a<0):
+            a = float(input("enter -ve no : "))
+            if(a<0):
+                n=n+1
+        print("count of -ve no is " , n)
+        while (a==0):
+            a = float(input("enter zero (O) : "))
+            if(a==0):
+                p=p+1
+        print("count of 0 is " , p)
+    else:
+        print("program ended")
+        break
 
 
 
